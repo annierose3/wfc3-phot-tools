218995702db6d7944b341f4705cfa454e91f86ce
Mariarosa Marinelli <mmarinelli@stsci.edu>
2022-08-19 16:00:41 -0400

    Making package name lowercase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name = 'WFC3_phot_tools',
+setup(name = 'wfc3_phot_tools',
       description = 'Python photometry tools for WFC3 calibration',
       author = 'Space Telescope Science Institute',
       url = 'https://github.com/spacetelescope/wfc3-phot-tools',
