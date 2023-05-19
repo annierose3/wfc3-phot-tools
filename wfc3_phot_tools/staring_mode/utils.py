fd7f82f5db2c0763d97394521fc6e25f76c5cba8
Mariarosa Marinelli <mmarinelli@stsci.edu>
2023-05-19 14:55:37 -0400

    minor reformatting and editing docstrings

--- wfc3_phot_tools/staring_mode/utils.py
+++ wfc3_phot_tools/staring_mode/utils.py
@@ -1,27 +1,23 @@
 """
-    This module contains utilities for performing aperture
-    photometry for staring mode observations.
+This module contains utilities for performing aperture
+photometry for staring mode observations.
 
-    Author
-    ------
-        Clare Shanahan, Dec 2019
-
-    To-Do
-    -----
-    - Figure out if query_simbad... needs return like (c)
-
+Authors
+-------
+    Clare Shanahan, December 2019
+    Mariarosa Marinelli, April 2023
 """
 
+import astropy.units as u
+import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
 from astropy.time import Time
-import astropy.units as u
 from astroquery.simbad import Simbad
-import numpy as np
 
 def query_simbad_by_targname(targname):
+    """Construct `SkyCoord` using SIMBAD query of target.
 
-    """
     Queries SIMBAD for `targname`, and constructs a
     `astropy.coordinates.SkyCoord` object containing the
     RA, Dec, proper motion, radial velocity, and distance
@@ -30,7 +26,7 @@ def query_simbad_by_targname(targname):
     Paramters
     ---------
     targname : str
-        Target name. Simbad is queried by target name.
+        Target name with which to query SIMBAD.
 
     Returns
     -------
@@ -42,26 +38,26 @@ def query_simbad_by_targname(targname):
     customSimbad.add_votable_fields('pmra', 'pmdec', 'distance', 'rv_value')
     result_table = customSimbad.query_object(targname)
 
-    RA = str(result_table['RA'].item())
-    RA = RA[0:2]+'h'+RA[3:5]+'m'+RA[6:]+'s'
+    simbad_ra = str(result_table['RA'].item())
+    ra = f'{simbad_ra[:2]}h{simbad_ra[3:5]}m{simbad_ra[6:]}s'
 
-    DEC = str(result_table['DEC'].item())
-    DEC = DEC[0:3]+'d'+DEC[4:6]+'m'+DEC[7:]+'s'
+    simbad_dec = str(result_table['DEC'].item())
+    dec = f'{simbad_dec[:3]}h{simbad_dec[4:6]}m{simbad_dec[7:]}s'
 
-    PM_RA = result_table['PMRA'].item()*u.mas/u.yr
-    PM_DEC = result_table['PMDEC'].item()*u.mas/u.yr
+    pm_ra = result_table['PMRA'].item()*u.mas/u.yr
+    pm_dec = result_table['PMDEC'].item()*u.mas/u.yr
 
-    c = SkyCoord(RA, DEC, frame='icrs', obstime=Time('J2000'),
+    c = SkyCoord(ra, dec, frame='icrs', obstime=Time('J2000'),
                  distance=result_table['Distance_distance'].item()*u.pc,
-                 pm_ra_cosdec=PM_RA,
-                 pm_dec=PM_DEC)
+                 pm_ra_cosdec=pm_ra,
+                 pm_dec=pm_dec)
 
-    return(c)
+    return c
 
 
 def apply_proper_motion_targ(targname, mjd):
+    """Apply PM to RA/Dec of target.
 
-    """
     Queries Simbad for ra, dec, and proper motions for
     'targname'. Returns (RA, and Dec) at date `mjd`
     considering the proper motion.
@@ -78,13 +74,11 @@ def apply_proper_motion_targ(targname, mjd):
     (ra_new, dec_new) : tuple of floats
         RA and Dec for `targname` after applying proper
         motion. In degrees.
-
     """
-
     c = query_simbad_by_targname(targname)
 
-    delta_t_yr = (mjd - 51544)/365. * u.yr
-    ra_new = c.ra.deg*u.deg+(c.pm_ra_cosdec * delta_t_yr)
-    dec_new = c.dec.deg*u.deg+(c.pm_dec * delta_t_yr)
+    delta_t_yr = (mjd - 51544) / 365. * u.yr
+    ra_new = c.ra.deg * u.deg + (c.pm_ra_cosdec * delta_t_yr)
+    dec_new = c.dec.deg * u.deg + (c.pm_dec * delta_t_yr)
 
-    return(ra_new.value, dec_new.value)
+    return ra_new.value, dec_new.value
